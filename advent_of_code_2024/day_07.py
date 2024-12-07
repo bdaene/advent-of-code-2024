eda3b996e68612783f6780bb9c952afe6c1d77cb
Benoît Daene <benoitdaene@gmail.com>
2024-12-07 22:25:42 +0100

    Improve Day 07
    (cherry picked from commit 7855bdd7156c5215b3c5cd8eeb2329bd7779c32c)

--- advent_of_code_2024/day_07.py
+++ advent_of_code_2024/day_07.py
@@ -1,5 +1,3 @@
-import operator
-from functools import cache
 from importlib.resources import files
 
 from advent_of_code_2024.utils import timeit, setup_logging
@@ -15,38 +13,40 @@ def get_data(input_file):
     return data
 
 
-@cache
-def get_values(numbers, operators):
+def check(value, numbers, concat=False):
     if len(numbers) == 1:
-        return {numbers[-1]}
-    value = numbers[-1]
+        return numbers[-1] == value
+
+    number = numbers[-1]
+    if value < number:
+        return False
     numbers = numbers[:-1]
-    values = get_values(numbers, operators)
-    new_values = set()
-    for op in operators:
-        new_values |= {op(v, value) for v in values}
-    return new_values
+
+    if check(value - number, numbers, concat):
+        return True
+
+    prev_value, r = divmod(value, number)
+    if r == 0 and check(prev_value, numbers, concat):
+        return True
+
+    if concat and value != number:
+        s_value, s_number = str(value), str(number)
+        if s_value.endswith(s_number):
+            prev_value = int(s_value.removesuffix(s_number))
+            if check(prev_value, numbers, concat):
+                return True
+
+    return False
 
 
 @timeit
 def part_1(data):
-    return sum(
-        value
-        for value, numbers in data
-        if value in get_values(numbers, (operator.add, operator.mul))
-    )
+    return sum(value for value, numbers in data if check(value, numbers))
 
 
 @timeit
 def part_2(data):
-    def concat(a, b):
-        return int(str(a) + str(b))
-
-    return sum(
-        value
-        for value, numbers in data
-        if value in get_values(numbers, (operator.add, operator.mul, concat))
-    )
+    return sum(value for value, numbers in data if check(value, numbers, True))
 
 
 def main():
