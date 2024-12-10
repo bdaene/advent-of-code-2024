a6f91cc8eb49953a9b81b93717d102de9ad80481
Benoît Daene <Benoit.Daene@kla.com>
2024-12-10 09:26:34 +0100

    Cleanup Day 10

--- advent_of_code_2024/day_10.py
+++ advent_of_code_2024/day_10.py
@@ -9,33 +9,7 @@ def get_data(input_file):
     return [list(map(int, line.strip())) for line in input_file]
 
 
-def get_score(grid, head):
-    assert grid[head[0]][head[1]] == 0
-    m, n = len(grid), len(grid[0])
-    to_visit = {head}
-    for height in range(1, 10):
-        to_visit_ = set()
-        for row, col in to_visit:
-            for dr, dc in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
-                row_, col_ = row + dr, col + dc
-                if 0 <= row_ < m and 0 <= col_ < n and grid[row_][col_] == height:
-                    to_visit_.add((row_, col_))
-        to_visit = to_visit_
-    return len(to_visit)
-
-
-@timeit
-def part_1(data):
-    total = 0
-    for row, line in enumerate(data):
-        for col, cell in enumerate(line):
-            if cell == 0:
-                total += get_score(data, (row, col))
-
-    return total
-
-
-def get_rating(grid, head):
+def get_tails(grid, head):
     assert grid[head[0]][head[1]] == 0
     m, n = len(grid), len(grid[0])
     to_visit = {head: 1}
@@ -47,7 +21,19 @@ def get_rating(grid, head):
                 if 0 <= row_ < m and 0 <= col_ < n and grid[row_][col_] == height:
                     to_visit_[(row_, col_)] += rating
         to_visit = to_visit_
-    return sum(to_visit.values())
+    return to_visit
+
+
+@timeit
+def part_1(data):
+    total = 0
+    for row, line in enumerate(data):
+        for col, cell in enumerate(line):
+            if cell == 0:
+                total += len(get_tails(data, (row, col)))
+
+    return total
+
 
 @timeit
 def part_2(data):
@@ -55,7 +41,7 @@ def part_2(data):
     for row, line in enumerate(data):
         for col, cell in enumerate(line):
             if cell == 0:
-                total += get_rating(data, (row, col))
+                total += sum(get_tails(data, (row, col)).values())
 
     return total
 
