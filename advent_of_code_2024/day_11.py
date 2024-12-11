08129bfdcc292c76a5be6db59f4cd58762199003
Benoît Daene <Benoit.Daene@kla.com>
2024-12-11 17:17:52 +0100

    Improve Day 11

--- advent_of_code_2024/day_11.py
+++ advent_of_code_2024/day_11.py
@@ -1,4 +1,5 @@
 from collections import Counter
+from functools import cache
 from importlib.resources import files
 
 from advent_of_code_2024.utils import timeit, setup_logging
@@ -9,34 +10,37 @@ def get_data(input_file):
     return list(map(int, input_file.readline().split()))
 
 
-def compute_after(stones, blinks):
+@cache
+def get_blink(stone):
+    if stone == 0:
+        return (1,)
+    stone_s = str(stone)
+    length, rest = divmod(len(stone_s), 2)
+    if rest:
+        return (stone * 2024,)
+    else:
+        return int(stone_s[:length]), int(stone_s[length:])
+
+
+def count_stones_after(stones, blinks):
     stones = Counter(stones)
-    for _ in range(blinks):
+    for blink in range(blinks):
         stones_ = Counter()
         for stone, count in stones.items():
-            if stone == 0:
-                stones_[1] += count
-                continue
-            stone_s = str(stone)
-            l, r = divmod(len(stone_s), 2)
-            if r:
-                stones_[stone * 2024] += count
-            else:
-                stones_[int(stone_s[:l])] += count
-                stones_[int(stone_s[l:])] += count
+            for stone_ in get_blink(stone):
+                stones_[stone_] += count
         stones = stones_
-
     return stones
 
 
 @timeit
 def part_1(data):
-    return sum(compute_after(data, 25).values())
+    return sum(count_stones_after(data, 25).values())
 
 
 @timeit
 def part_2(data):
-    return sum(compute_after(data, 75).values())
+    return sum(count_stones_after(data, 75).values())
 
 
 def main():
