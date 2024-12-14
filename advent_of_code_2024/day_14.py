422e2889c2f308e5cadb82a77af956ba644cbcc3
Benoît Daene <benoitdaene@gmail.com>
2024-12-14 13:51:18 +0100

    Day 14

---
+++ advent_of_code_2024/day_14.py
@@ -0,0 +1,63 @@
+import re
+from collections import Counter
+from importlib.resources import files
+from itertools import count
+
+from advent_of_code_2024.utils import timeit, setup_logging
+
+PATTERN = re.compile(r"p=(-?\d+),(-?\d+) v=(-?\d+),(-?\d+)")
+
+
+@timeit
+def get_data(input_file):
+    data = []
+    for line in input_file:
+        px, py, vx, vy = map(int, PATTERN.fullmatch(line.strip()).groups())
+        data.append(((px, py), (vx, vy)))
+    return data
+
+
+@timeit
+def part_1(data, tiles=(101, 103), time=100):
+    counter = Counter()
+    tx, ty = tiles
+    for (px, py), (vx, vy) in data:
+        px = (px + vx * time) % tx
+        py = (py + vy * time) % ty
+        qx = px - tx // 2
+        qy = py - ty // 2
+        qx = 0 if qx == 0 else (-1 if qx < 0 else 1)
+        qy = 0 if qy == 0 else (-1 if qy < 0 else 1)
+        counter[(qx, qy)] += 1
+
+    return counter[(-1, -1)] * counter[(-1, 1)] * counter[(1, -1)] * counter[(1, 1)]
+
+
+@timeit
+def part_2(data, tiles=(101, 103), pattern="#" * 16):
+    tx, ty = tiles
+    for time in count(1):
+        positions = set(
+            ((px + vx * time) % tx, (py + vy * time) % ty)
+            for (px, py), (vx, vy) in data
+        )
+        s = "\n".join(
+            "".join("#" if (px, py) in positions else "." for px in range(tx))
+            for py in range(ty)
+        )
+        if pattern in s:
+            print(time)
+            print(s)
+            return time
+
+
+def main():
+    setup_logging()
+    with (files("data.inputs") / "day_14.txt").open() as input_file:
+        data = get_data(input_file)
+    part_1(data)
+    part_2(data)
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_day_14.py
@@ -0,0 +1,37 @@
+from importlib.resources import files
+
+from advent_of_code_2024.day_14 import get_data, part_1, part_2
+
+DATA = [
+    ((0, 4), (3, -3)),
+    ((6, 3), (-1, -3)),
+    ((10, 3), (-1, 2)),
+    ((2, 0), (2, -1)),
+    ((0, 0), (1, 3)),
+    ((3, 0), (-2, -2)),
+    ((7, 6), (-1, -3)),
+    ((3, 0), (-1, -2)),
+    ((9, 3), (2, 3)),
+    ((7, 3), (-1, 2)),
+    ((2, 4), (2, -3)),
+    ((9, 5), (-3, -3)),
+]
+
+
+def test_get_data():
+    with (files("data.samples") / "day_14.txt").open() as input_file:
+        data = get_data(input_file)
+
+    assert data == DATA
+
+
+def test_part_1():
+    result = part_1(DATA, tiles=(11, 7))
+
+    assert result == 12
+
+
+def test_part_2():
+    result = part_2(DATA, tiles=(11, 7), pattern="#" * 4)
+
+    assert result == 6
