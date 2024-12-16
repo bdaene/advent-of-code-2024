b00cf4e493903d883f39abe28c813d9902d13d92
Benoît Daene <benoitdaene@gmail.com>
2024-12-16 12:27:08 +0100

    Cleanup Day 16

--- advent_of_code_2024/day_16.py
+++ advent_of_code_2024/day_16.py
@@ -1,4 +1,3 @@
-from heapq import heappop, heappush
 from importlib.resources import files
 
 from advent_of_code_2024.utils import timeit, setup_logging
@@ -9,59 +8,22 @@ def get_data(input_file):
     return [line.strip() for line in input_file]
 
 
-def dist(a, b):
-    ar, ac = a
-    br, bc = b
-    return abs(br - ar) + abs(bc - ac)
-
-
-INF = 10**9 + 7
-
-
-@timeit
-def part_1(data):
-    grid = data
-
-    start = next((row, col) for row, line in enumerate(grid) for col, cell in enumerate(line) if cell == "S")
-    target = next((row, col) for row, line in enumerate(grid) for col, cell in enumerate(line) if cell == "E")
-    print(start, target)
-
-    start_direction = (0, 1)
-    to_visit = [(dist(start, target), 0, start, (0, 1))]
-    seen = {(start, start_direction): 0}
-    while to_visit:
-        # print(to_visit)
-        _, cost, position, direction = heappop(to_visit)
-        if position == target:
-            return cost
-        (row, col), (dr, dc) = position, direction
-        for position_, direction_, cost_ in [
-            ((row + dr, col + dc), direction, cost + 1),
-            (position, (-dc, dr), cost + 1000),
-            (position, (dc, -dr), cost + 1000),
-        ]:
-            row_, col_ = position_
-            if grid[row_][col_] != "#" and seen.get((position_, direction_), INF) > cost:
-                seen[(position_, direction_)] = cost
-                heappush(to_visit, (dist(position_, target) + cost_, cost_, position_, direction_))
-
-
+INF = 10**9 - 1
 DIRECTIONS = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
 
-@timeit
-def part_2(data):
-    grid = data
+def extract_position(grid, cell_value):
+    return next((row, col) for row, line in enumerate(grid) for col, cell in enumerate(line) if cell == cell_value)
 
-    start = next((row, col) for row, line in enumerate(grid) for col, cell in enumerate(line) if cell == "S")
-    target = next((row, col) for row, line in enumerate(grid) for col, cell in enumerate(line) if cell == "E")
-    print(start, target)
+
+def compute_costs(grid, start):
+    start_row, start_col = start
+
+    costs = [[[INF] * 4 for cell in line] for line in grid]  # costs[row][col]dir]
+    costs[start_row][start_col][0] = 0
 
     to_visit = [(start, 0, 0)]
-    costs = [[[INF] * 4 for cell in line] for line in grid]
-    costs[start[0]][start[1]][0] = 0
     while to_visit:
-        # print(to_visit)
         to_visit_ = []
         for position, direction, cost in to_visit:
             (row, col), (dr, dc) = position, DIRECTIONS[direction]
@@ -76,21 +38,42 @@ def part_2(data):
                     to_visit_.append((position_, direction_, cost_))
         to_visit = to_visit_
 
-    target_cost = min(costs[target[0]][target[1]][direction] for direction in range(4))
+    return costs
 
+
+@timeit
+def part_1(data):
+    grid = data
+
+    start = extract_position(grid, "S")
+    end = extract_position(grid, "E")
+
+    costs = compute_costs(grid, start)
+
+    end_row, end_col = end
+    return min(costs[end_row][end_col][direction] for direction in range(4))
+
+
+@timeit
+def part_2(data):
+    grid = data
+
+    start = extract_position(grid, "S")
+    end = extract_position(grid, "E")
+
+    costs = compute_costs(grid, start)
     # print('\n'.join(''.join(str(cost % 10) if (cost := min(cells)) < INF else '#' for cells in line) for line in costs))
 
-    cells = {
-        (target, direction, target_cost)
-        for direction in range(4)
-        if costs[target[0]][target[1]][direction] == target_cost
-    }
+    end_row, end_col = end
+    end_cost = min(costs[end_row][end_col][direction] for direction in range(4))
+
     best_places = set()
-    while cells:
-        # print(cells)
-        best_places |= {position for position, direction, cost in cells}
-        cells_ = set()
-        for position, direction, cost in cells:
+    best_paths = {(end, direction, end_cost) for direction in range(4) if costs[end[0]][end[1]][direction] == end_cost}
+    while best_paths:
+        # print(best_paths)
+        best_places |= {position for position, direction, cost in best_paths}
+        best_paths_ = set()
+        for position, direction, cost in best_paths:
             (row, col), (dr, dc) = position, DIRECTIONS[direction]
             for position_, direction_, cost_ in [
                 ((row - dr, col - dc), direction, cost - 1),
@@ -99,8 +82,8 @@ def part_2(data):
             ]:
                 (row_, col_) = position_
                 if costs[row_][col_][direction_] == cost_:
-                    cells_.add((position_, direction_, cost_))
-        cells = cells_
+                    best_paths_.add((position_, direction_, cost_))
+        best_paths = best_paths_
 
     # print('\n'.join(''.join('O' if (row, col) in best_places else cell for col, cell in enumerate(line)) for row, line in enumerate(grid)))
 
