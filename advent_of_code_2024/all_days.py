c12dba75c9b489166d2333fd54a95acaba5c7992
Benoît Daene <benoitdaene@gmail.com>
2024-12-23 21:38:48 +0100

    Fix importlib deprecation.

--- advent_of_code_2024/all_days.py
+++ advent_of_code_2024/all_days.py
@@ -1,6 +1,6 @@
 import logging
 from importlib import import_module
-from importlib.resources import contents
+from importlib.resources import files
 
 from utils import timeit
 
@@ -8,7 +8,8 @@ _LOGGER = logging.getLogger(__name__)
 
 
 def get_all_days():
-    all_days = [file.removesuffix(".py") for file in contents("advent_of_code_2024") if file.startswith("day_")]
+    all_files = [file.name for file in files("advent_of_code_2024").iterdir()]
+    all_days = [file.removesuffix(".py") for file in all_files if file.startswith("day_")]
     return {day: import_module(day, "advent_of_code_2024") for day in all_days}
 
 
