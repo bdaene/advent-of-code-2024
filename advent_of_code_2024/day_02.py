e0d1a95bdfb66734d93fdfba431f5ec974933e43
Benoît Daene <benoitdaene@gmail.com>
2025-05-31 16:23:30 +0200

    Improve Day 02

--- advent_of_code_2024/day_02.py
+++ advent_of_code_2024/day_02.py
@@ -1,5 +1,4 @@
 from importlib.resources import files
-from itertools import pairwise
 
 from advent_of_code_2024.utils import timeit, setup_logging
 
@@ -9,26 +8,35 @@ def get_data(input_file):
     return [list(map(int, line.split())) for line in input_file]
 
 
-def is_safe(report):
-    if all(1 <= b - a <= 3 for a, b in pairwise(report)):
-        return True
-    if all(1 <= a - b <= 3 for a, b in pairwise(report)):
-        return True
-    return False
+def is_safe(report, allowed_bad_levels=0):
+    report = reversed(report)
+    last_level = next(report)
+    for level in report:
+        if 1 <= level - last_level <= 3:
+            last_level = level
+        elif allowed_bad_levels <= 0:
+            return False
+        else:
+            allowed_bad_levels -= 1
+    return True
 
 
 @timeit
 def part_1(data):
-    return sum(is_safe(report) for report in data)
+    return sum(is_safe(report) or is_safe(report[::-1]) for report in data)
 
 
 @timeit
-def part_2(data):
+def part_2(data, allowed_bad_levels=1):
     count = 0
     for report in data:
-        if any(is_safe(report[:i] + report[i + 1 :]) for i in range(len(report))):
-            count += 1
-
+        reversed_report = report[::-1]
+        for skip in range(allowed_bad_levels + 1):
+            if is_safe(report, allowed_bad_levels - skip) or is_safe(reversed_report, allowed_bad_levels - skip):
+                count += 1
+                break
+            report.pop()
+            reversed_report.pop()
     return count
 
 
